362f8990904447576e5c089c22ba9225628babd8
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-09 23:53:58 -0400

    Create swapNodes method for DLL

--- Assignment_1/Q2 b/DoubleLinkedList.java
+++ Assignment_1/Q2 b/DoubleLinkedList.java
@@ -1,3 +1,55 @@
 public class DoubleLinkedList{
-    
+
+    private Node head;
+    private Node tail;
+
+    public void swapNodes(Node firstNode, Node secondNode){
+        
+        Node beforeFirst = firstNode.getPrevious();
+        Node afterSecond = secondNode.getNext();
+
+        if (firstNode == head){
+            secondNode.setPrevious(null);
+            head = secondNode;
+        }
+        else if (secondNode == tail){
+            firstNode.setNext(null);
+            beforeFirst.setNext(secondNode);
+            tail = firstNode;
+        }
+        else{
+            secondNode.setPrevious(beforeFirst);
+            beforeFirst.setNext(secondNode);
+            afterSecond.setPrevious(firstNode);
+        }
+
+        firstNode.setNext(afterSecond);
+        firstNode.setPrevious(secondNode);
+        secondNode.setNext(firstNode);
+    }
+
+    public void addToFront(Node newNode){
+        
+        newNode.setNext(head);
+        if (head == null){
+            tail = newNode;
+        }
+        else{
+            head.setPrevious(newNode);
+            newNode.setNext(head);
+        }
+        head = newNode;
+    }
+
+    public void printNodes(){
+        Node current = head;
+        System.out.print("HEAD -> ");
+
+        while(current != null){
+            System.out.print(current.getValue());
+            current = current.getNext();
+            System.out.print(" <=> ");
+        }
+        System.out.println("null \n");
+    }
 }
--- Assignment_1/Q2 b/Main.java
+++ Assignment_1/Q2 b/Main.java
@@ -1,6 +1,33 @@
 
 public class Main{
     public static void main(String[] args) {
-        
+        DoubleLinkedList dll = new DoubleLinkedList();
+
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+        dll.addToFront(node1);
+        dll.addToFront(node2);
+        dll.addToFront(node3);
+        dll.addToFront(node4);
+        dll.addToFront(node5);
+
+        System.out.println("-----Before Swapping-----");
+        dll.printNodes();
+
+        System.out.println("------Swap Node 4 and Node 3-----");
+        dll.swapNodes(node4, node3);
+        dll.printNodes();
+
+        System.out.println("-------Swap Node 5 and Node 3-------");
+        dll.swapNodes(node5, node3);
+        dll.printNodes();
+
+        System.out.println("-------Swap Node 1 and Node 2");
+        dll.swapNodes(node2, node1);
+        dll.printNodes();
     }
 }
