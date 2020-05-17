7271a432042502eabbd478ac6fcf13a3b2f38d7e
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-16 22:42:10 -0400

    add deleteMin() method

--- Assignment_1/Q1 a/Main.java
+++ Assignment_1/Q1 a/Main.java
@@ -1,6 +1,29 @@
 public class Main{
     public static void main(String[] args) {
         
-        
+        MyPriortyQueue priortyQueue = new MyPriortyQueue();
+
+        priortyQueue.add(2);
+        priortyQueue.add(10);
+        priortyQueue.add(1);
+        priortyQueue.add(22);
+        priortyQueue.add(3);
+
+        System.err.println("Before Removing: ");
+        priortyQueue.printNodes();
+
+        System.out.println("One Item Removed");
+        priortyQueue.deleteMin();
+        priortyQueue.printNodes();
+
+        System.out.println("One Item Removed");
+        priortyQueue.deleteMin();
+        priortyQueue.printNodes();
+
+        System.out.println("One Item Removed");
+        priortyQueue.deleteMin();
+        priortyQueue.printNodes();
+
+        System.out.println("The size of the queue after removing items is " + priortyQueue.size());
     }
 }
--- Assignment_1/Q1 a/MyPriortyQueue.java
+++ Assignment_1/Q1 a/MyPriortyQueue.java
@@ -1,6 +1,7 @@
 public class MyPriortyQueue{
 
     private Node head;
+    private int size;
 
     public void add(int number){
 
@@ -17,17 +18,70 @@ public class MyPriortyQueue{
             
             lastNode.setNext(newNode);
         }
+        size++;
+    }
 
+    public void deleteMin(){
+
+        Node current = head.getNext();
+        int min = head.getNumber();
+        Node removedNode = head;
+        Node previous = head;
+        Node previous2 = head;
+
+        if (size() == 1){
+            head = null;
+            return; 
+        }
+
+        while(current.getNext() != null){
+
+            if (current.getNumber() < min){
+                min = current.getNumber();
+                removedNode = current;
+                previous = previous.getNext();
+            }
+
+            current = current.getNext();
+            previous2 = previous2.getNext();
+
+            if (current.getNext() == null && current.getNumber() < min){
+                removedNode = current;
+                previous = previous2;
+            }
+            
+
+        }
+
+        if (removedNode == head){
+            head = head.getNext();
+            removedNode.setNext(null);
+        } 
+        else if (removedNode.getNext() == null){
+            previous.setNext(null);
+        }
+        else{
+            previous.setNext(removedNode.getNext());
+            removedNode.setNext(null);
+        }
+        size--;
+
+    }
+
+    public int size(){
+        return size;
     }
 
     public void printNodes(){
         Node current = head;
         
+        System.out.print("[ ");
         while (current.getNext() != null){
             System.out.print(current.getNumber() + " ");
             current = current.getNext();
         }
-        System.out.println();
+        System.out.print(current.getNumber() + " ");
+        System.out.print("]\n");
     }
 
 }
