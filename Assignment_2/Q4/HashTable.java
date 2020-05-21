93bbf3930caf1f6c7f8ce4929f0f8dece071c142
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-20 22:30:57 -0400

    add comments

--- Assignment_2/Q4/HashTable.java
+++ Assignment_2/Q4/HashTable.java
@@ -4,7 +4,7 @@ public class HashTable{
     private int maxSize; // size of the hash table
 
     /**
-     * 
+     * This constructor initialize the hash array
      * @param maxSize
      */
     public HashTable(int maxSize){
@@ -13,46 +13,57 @@ public class HashTable{
     }
 
     /**
-     * 
-     * @param key
+     * add/put new item in the hash table
+     * @param key the key and the item to add in the hash table
      */
     public void put(int key){
-        int hashedKey = hashKey(key);
+        int hashedKey = hashKey(key); // get the index
 
-        if(isOccupied(hashedKey)){
+        // if the hash table occupied at certain position
+        if(isOccupied(hashedKey)){ 
+            int stopIndex = hashedKey; // index where the loop has if the array is full
+            // if we reached the end of array, then start at 0 index
             if(hashedKey == hashArray.length - 1){
                 hashedKey = 0;
             }
             else{
                 hashedKey++;
             }
-
-            while(isOccupied(hashedKey)){
+            // keep looping as long as array is occupied and array is not full
+            while(isOccupied(hashedKey) && hashedKey != stopIndex){
                 hashedKey = (hashedKey + 1) % hashArray.length;
             }
         }
-
+        // this condition prints when the array is full
         if (hashArray[hashedKey] != null){
-            System.out.println("There's already an number at position " + hashedKey);
+            System.out.println("Hash table is full! You can't add " + key);
         }
+        //add the an item at a the hashedKey index
         else{
             hashArray[hashedKey] = key;
+            System.out.println(key + " has been added to the hash table at position " + hashedKey);
         }
     }
 
     /**
-     * 
-     * @param key
-     * @return
+     * return the index of the new element
+     * @param key the value that will be converted to index
+     * @return index of the new element
      */
     private int hashKey(int key){
 
         return key % 13;
     }
 
+    /**
+     * check whether a position is occupied or not
+     * @param index 
+     * @return true if the a position isn't empty
+     */
     private boolean isOccupied(int index){
         return hashArray[index] != null;
     }
+    
     /**
      * print the elements in the hash table.
      */
--- Assignment_2/Q4/Main.java
+++ Assignment_2/Q4/Main.java
@@ -1,15 +1,16 @@
 public class Main{
     public static void main(String[] args) {
         
+        //create new array
         int[] array = {1,5,21,26,39,14,15,16,17,18,19,20,111,145,146};
 
-        HashTable hashTable = new HashTable(15);
+        // create an empty hash table
+        HashTable hashTable = new HashTable(13);
 
+        // elements in the array into the hashtable
         for(int i=0; i<array.length; i++){
             hashTable.put(array[i]);
         }
-
-        hashTable.printHashTable();
 
     }
 }
