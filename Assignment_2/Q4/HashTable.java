b20c1d99711e6ba8701baccd43e48c4b15689bd6
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-20 20:43:32 -0400

    test inital hash table

--- Assignment_2/Q4/HashTable.java
+++ Assignment_2/Q4/HashTable.java
@@ -1,9 +1,26 @@
 public class HashTable{
 
     private int[] hashArray;
-    private int maxSize
+    private int maxSize;
 
     public HashTable(int maxSize){
+        this.maxSize = maxSize;
+        hashArray = new int[maxSize];
+    }
 
+    public void put(int key){
+        int hashKey = hashKey(key);
+        hashArray[hashKey] = key;
+    }
+
+    private int hashKey(int key){
+
+        return key % 13;
+    }
+
+    public void printHashTable(){
+        for (int i=0; i<hashArray.length; i++){
+            System.out.println(hashArray[i]);
+        }
     }
 }
--- Assignment_2/Q4/Main.java
+++ Assignment_2/Q4/Main.java
@@ -1,6 +1,15 @@
 public class Main{
     public static void main(String[] args) {
         
-        
+        int[] array = {1,5,21,26,39};
+
+        HashTable hashTable = new HashTable(10);
+
+        for(int i=0; i<array.length; i++){
+            hashTable.put(array[i]);
+        }
+
+        hashTable.printHashTable();
+
     }
 }
