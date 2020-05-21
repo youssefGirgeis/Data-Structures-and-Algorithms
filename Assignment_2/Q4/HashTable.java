1f52b05b39409c5188cbca81aed5aff8606334d5
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-20 21:38:49 -0400

    complete put() method for the hashtable

--- Assignment_2/Q4/HashTable.java
+++ Assignment_2/Q4/HashTable.java
@@ -1,23 +1,61 @@
 public class HashTable{
 
-    private int[] hashArray;
-    private int maxSize;
+    private Integer[] hashArray; //backing array for the hash table
+    private int maxSize; // size of the hash table
 
+    /**
+     * 
+     * @param maxSize
+     */
     public HashTable(int maxSize){
         this.maxSize = maxSize;
-        hashArray = new int[maxSize];
+        hashArray = new Integer[maxSize];
     }
 
+    /**
+     * 
+     * @param key
+     */
     public void put(int key){
-        int hashKey = hashKey(key);
-        hashArray[hashKey] = key;
+        int hashedKey = hashKey(key);
+
+        if(isOccupied(hashedKey)){
+            if(hashedKey == hashArray.length - 1){
+                hashedKey = 0;
+            }
+            else{
+                hashedKey++;
+            }
+
+            while(isOccupied(hashedKey)){
+                hashedKey = (hashedKey + 1) % hashArray.length;
+            }
+        }
+
+        if (hashArray[hashedKey] != null){
+            System.out.println("There's already an number at position " + hashedKey);
+        }
+        else{
+            hashArray[hashedKey] = key;
+        }
     }
 
+    /**
+     * 
+     * @param key
+     * @return
+     */
     private int hashKey(int key){
 
         return key % 13;
     }
 
+    private boolean isOccupied(int index){
+        return hashArray[index] != null;
+    }
+    /**
+     * print the elements in the hash table.
+     */
     public void printHashTable(){
         for (int i=0; i<hashArray.length; i++){
             System.out.println(hashArray[i]);
--- Assignment_2/Q4/Main.java
+++ Assignment_2/Q4/Main.java
@@ -1,9 +1,9 @@
 public class Main{
     public static void main(String[] args) {
         
-        int[] array = {1,5,21,26,39};
+        int[] array = {1,5,21,26,39,14,15,16,17,18,19,20,111,145,146};
 
-        HashTable hashTable = new HashTable(10);
+        HashTable hashTable = new HashTable(15);
 
         for(int i=0; i<array.length; i++){
             hashTable.put(array[i]);
