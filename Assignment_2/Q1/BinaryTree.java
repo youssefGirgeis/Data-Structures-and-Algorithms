dfbe2d7a57d18ef4c8266b6e08831dd599e98e52
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-23 20:17:00 -0400

    add nodes to the Binary Search Tree

--- Assignment_2/Q1/BinaryTree.java
+++ Assignment_2/Q1/BinaryTree.java
@@ -1,3 +1,13 @@
 public class BinaryTree{
     
+    private Node root;
+
+    public void insert(int value){
+        if (root == null){
+            root = new Node(value);
+        }
+        else{
+            root.insert(value);
+        }
+    }
 }
--- Assignment_2/Q1/Main.java
+++ Assignment_2/Q1/Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         
-        
+        BinaryTree bt = new BinaryTree();
+
+        bt.insert(26);
     }
 }
