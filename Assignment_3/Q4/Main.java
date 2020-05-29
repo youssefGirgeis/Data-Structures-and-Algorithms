e6853716b82bc4c301a276224dcf08b70d1407ed
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-28 23:07:57 -0400

    Implement quick sort algorithm

--- Assignment_3/Q4/Main.java
+++ Assignment_3/Q4/Main.java
@@ -4,15 +4,43 @@ public class Main {
         
         int[] unorderedArray = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
 
-        MergeSort sorter = new MergeSort();
-        sorter.sort(unorderedArray);
-
-        for(int i=0; i<unorderedArray.length; i++){
-           
-            System.out.print(unorderedArray[i] + ", ");
+        System.out.print("Array before sorting: ");
+        for (int i = 0; i < unorderedArray.length; i++) {
+            System.out.print(unorderedArray[i] + " ");
         }
         System.out.println();
 
+        MergeSort sorter = new MergeSort();
+        sorter.sort(unorderedArray, 0, unorderedArray.length-1);
+
+        System.out.print("Merge Sort: ");
+        for(int i=0; i<unorderedArray.length; i++){
+           
+            System.out.print(unorderedArray[i] + " ");
+        }
+        System.out.println();
+
+
+
+
+    
+        int [] unsortedArray = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+
+        System.out.print("Array before sorting: ");
+        for (int i = 0; i < unsortedArray.length; i++) {
+            System.out.print(unsortedArray[i] + " ");
+        }
+        System.out.println();
+
+        QuickSort qSorter = new QuickSort();
+        qSorter.quickSort(unsortedArray, 0, unsortedArray.length-1);
+
+        System.out.print("Quick Sort: ");
+        for(int i=0; i<unsortedArray.length; i++){
+           
+            System.out.print(unsortedArray[i] + " ");
+        }
+        System.out.println();
     }
     
 }
---
+++ Assignment_3/Q4/QuickSort.java
@@ -0,0 +1,34 @@
+public class QuickSort {
+
+    public void quickSort(int[] unsortedArray, int start, int end){
+        if (start < end){
+            int partitionIndex = partition(unsortedArray, start, end); // index position of the correctly placed pivot in the array
+            quickSort(unsortedArray, start, partitionIndex-1); // to sort the left partition
+            quickSort(unsortedArray, partitionIndex+1, end); // sort the right partition
+        }
+    }
+
+    private int partition(int[] unsortedArray, int start, int end){
+        
+        int pivot = unsortedArray[end];
+        int i = start - 1; // i starts at -1 in the first iteration
+        for (int j= start; j <= end-1; j++){
+            if(unsortedArray[j] < pivot){
+                i++;
+                int iTemp = unsortedArray[i];
+                int jTemp = unsortedArray[j];
+                //swap
+                unsortedArray[i] = jTemp;
+                unsortedArray[j] = iTemp;
+            }
+        }
+
+        //place the pivot value in the correct position
+        int nextToI = unsortedArray[i+1]; // pivot's position
+        unsortedArray[end] = nextToI; // move the elment at the pivot position to the end
+        unsortedArray[i+1] = pivot; // move pivot the correct spot.
+
+        return i+1;
+    }
+    
+}
