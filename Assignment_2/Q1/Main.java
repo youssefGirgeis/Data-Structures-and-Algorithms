76acf82b0570082e2c6e055f26bf095a65e57ffe
Youssef Girgeis <YoussefGirgeis@gmail.com>
2020-05-31 02:01:48 -0400

    add comments and javadoc to Q1

--- Assignment_2/Q1/Main.java
+++ Assignment_2/Q1/Main.java
@@ -1,12 +1,30 @@
+/*
+Course: COMP 272
+Assignment: 2 - Q1
+Name: Youssef Girgeis
+Student ID : 3396678
+Date: May 31, 2020
+
+Problem:
+Design an algorithm for the following operations for a binary tree BT, and show the
+worst-case running times for each implementation:
+preorderNext(x): return the node visited after node x in a pre-order traversal of BT.
+postorderNext(x): return the node visited after node x in a post-order traversal of BT.
+inorderNext(x): return the node visited after node x in an in-order traversal of BT
+*/
+
 
 public class Main {
     public static void main(String[] args) {
 
+        // create a tree
         TreeNode node = new TreeNode();
         
+        //create the root
         TreeNode root = node.insert(25);
         root.setParent(null);
 
+        //create children
         root.setLeft(root.insert(20));
         root.getLeft().setParent(root);
 
@@ -31,36 +49,47 @@ public class Main {
         root.getRight().getRight().setRight(root.insert(32));
         root.getRight().getRight().getRight().setParent(root.getRight().getRight());
 
+        // get the next node in the preorder sequence
+        TreeNode result = root.preOrderNext(root.getRight().getRight().getLeft()); 
 
-        TreeNode result = root.preOrderNext(root.getRight().getRight().getLeft());
+        //get the next node in the post order sequence
         TreeNode result2 = root.postOrderNext(root.getLeft());
+
+        // get the next node in the inorder sequence
         TreeNode result3 = root.inOrderNext(root.getLeft().getRight());
-  
+        
+        // print the next nodes for all preorder, post order, and inorder
         if (result != null)  
         { 
-            System.out.printf("Pre Order Next of %d is %d\n", root.getRight().getRight().getLeft().getData(), result.getData()); 
+            System.out.printf("The node after %d in a pre-order traversal is %d\n", 
+            root.getRight().getRight().getLeft().getData(), result.getData()); 
         } 
         else
         { 
-            System.out.printf("Next of %d is null\n", root.getRight().getRight().getLeft().getData()); 
+            System.out.printf("Next of %d in a pre-order traversal is null\n", 
+            root.getRight().getRight().getLeft().getData()); 
         }
 
         if (result2 != null)  
         { 
-            System.out.printf("Post Order Next of %d is %d\n", root.getLeft().getData(), result2.getData()); 
+            System.out.printf("The node after %d in a post-order traversal is %d\n", 
+            root.getLeft().getData(), result2.getData()); 
         } 
         else
         { 
-            System.out.printf("Next of %d is null\n", root.getLeft().getData()); 
+            System.out.printf("Next of %d in post-order traversal is null\n", 
+            root.getLeft().getData()); 
         }
 
         if (result3 != null)  
         { 
-            System.out.printf("In Order Next of %d is %d\n", root.getLeft().getRight().getData(), result3.getData()); 
+            System.out.printf("The node after %d in an in-order traversal is %d\n", 
+            root.getLeft().getRight().getData(), result3.getData()); 
         } 
         else
         { 
-            System.out.printf("Next of %d is null\n", root.getLeft().getRight().getData()); 
+            System.out.printf("Next of %d in an in-order traversal  is null\n", 
+            root.getLeft().getRight().getData()); 
         }
 
     }
--- Assignment_2/Q1/TreeNode.java
+++ Assignment_2/Q1/TreeNode.java
@@ -1,69 +1,98 @@
 
+/*
+Course: COMP 272
+Assignment: 2 - Q1
+Name: Youssef Girgeis
+Student ID : 3396678
+Date: May 31, 2020
+
+Problem:
+Design an algorithm for the following operations for a binary tree BT, and show the
+worst-case running times for each implementation:
+preorderNext(x): return the node visited after node x in a pre-order traversal of BT.
+postorderNext(x): return the node visited after node x in a post-order traversal of BT.
+inorderNext(x): return the node visited after node x in an in-order traversal of BT
+*/
+
 
 public class TreeNode {
     
-    private int data;
-    private TreeNode parent;
-    private TreeNode leftChild;
-    private TreeNode rightChild;
+    private int data; // value stored in the node
+    private TreeNode parent; // parent of node
+    private TreeNode leftChild; // left child node
+    private TreeNode rightChild; // right child node
 
+    /**
+     * creates new node and sets the left child and
+     * right child to null
+     * @param value new node's value
+     * @return the new node
+     */
     public TreeNode insert(int value){
         TreeNode node = new TreeNode();
-        node.leftChild = null;
+        node.leftChild = null; 
         node.rightChild = null;
         node.parent = null;
         node.data = value;
         return node;
     }
 
+    /**
+     * return the node visited after node in a pre-order traversal of BT
+     * @param node the node before target in a pre-order traversal of BT
+     * @return the target node
+     */
     public TreeNode preOrderNext(TreeNode node){
 
-        // If left child exists, then it is preorder 
-        // successor. 
         if (node.leftChild != null) 
             return node.leftChild; 
 
-        // If left child does not exist, then 
-        // travel up (using parent pointers) 
-        // until we reach a node which is left 
-        // child of its parent. 
-        TreeNode curr = node, parent = curr.parent; 
-        while (parent != null && parent.rightChild == curr)  
+        TreeNode current = node;
+        parent = current.parent; 
+        while (parent != null && parent.rightChild == current)  
         { 
-            curr = curr.parent; 
+            current = current.parent; 
             parent = parent.parent; 
         } 
 
-        // If we reached root, then the given 
-        // node has no preorder successor 
+        // if root then no next and return null
         if (parent == null) 
             return null; 
 
         return parent.rightChild; 
     }
 
+    /**
+     * returns the node visited after node in a post-order traversal of BT
+     * @param node node before target in a post-order traversal of BT
+     * @return the target node
+     */
     public TreeNode postOrderNext(TreeNode node){
-        // Root has no successor in postorder  
-        // traversal  
+
+        // if root, then no next node and return null.
         if (node.parent == null)  
             return null;  
 
-        // If given node is right child of its  
-        // parent or parent's right is empty, then  
-        // parent is postorder successor.  
+        // root is the target if node is the right child
+        //or root's right is empty
         TreeNode parent = node.parent;  
         if (parent.rightChild == null || parent.rightChild == node)  
             return parent;  
 
-        // In all other cases, find the leftmost  
-        // child in right substree of parent.  
-        TreeNode curr = parent.rightChild;  
-        while (curr.leftChild != null)  
-            curr = curr.leftChild;  
+        
+        // return leftmost child in right sub-tree
+        TreeNode current = parent.rightChild;  
+        while (current.leftChild != null)  
+        current = current.leftChild;  
 
-        return curr; 
+        return current; 
     }
 
+    /**
+     * returns the node visited after node in an in-order traversal of BT
+     * @param node node before target in an in-order traversal of BT
+     * @return target node
+     */
     public TreeNode inOrderNext(TreeNode node){
 
         if (node.rightChild != null){
@@ -82,6 +111,8 @@ public class TreeNode {
         return p;
     }
 
+    // below are the setters and getters for the TreeNode's fields
+    
     public void setParent(TreeNode parent){
         this.parent = parent;
     }
